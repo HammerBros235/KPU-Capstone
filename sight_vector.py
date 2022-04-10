d3fba2d67d1b3bcf5a65c6ef3177f0762b9bec69
HammerBros235 <84055441+HammerBros235@users.noreply.github.com>
2022-04-10 21:49:15 +0900

    Delete sight_vector.py

--- sight_vector.py
+++
@@ -1,5 +0,0 @@
-#Consider that the all facials are all connected. It may not need to be tracked individually.
-#So, I might not have to calculate the vectors, but instead just record the positions of each, and see how often it moves away.
-#Then all I can do is detect sudden movement?
-#Ref that one github project anyway.
-#or maybe we can reverse intergrate?
