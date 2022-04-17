b69751b60ce804ee4fc4003d0db3ccdb14e1dd50
HammerBros235 <84055441+HammerBros235@users.noreply.github.com>
2022-04-17 23:59:24 +0900

    Update AttFuncs.py

--- AttFuncs.py
+++ AttFuncs.py
@@ -1,9 +1,9 @@
-import Point
+#import Point
 
-def Shakiness(pD):
-    #Boundary percentage. Arbitarily chosen to be 10 for now.
+def shakiness(pD):
+    #정도 조정. 임의적으로 10%으로 정함.
     limit = 10
     if (abs(pD.x) > limit or abs(pD.y) > limit or abs(pD.w) > limit or abs(pD.h) > limit):
-        print("[Shakiness] Shaking!")
+        print("[Shakiness] 흔들림!")
     else:
-        print("[Shakiness] steady...")
+        print("[Shakiness] 가만함...")
