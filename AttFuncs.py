3a3321d5637cf7a310ef976d10ca7d94b1b46b33
HammerBros235 <84055441+HammerBros235@users.noreply.github.com>
2022-04-23 18:26:25 +0900

    Update AttFuncs.py

--- AttFuncs.py
+++ AttFuncs.py
@@ -1,9 +1,22 @@
-#import Point
+from Point import Point
+
+#화면크기: 720p
+pScreenX = 1280
+pScreenY = 720
+
+#차이% 계산
+def pDiff(p1, p2):
+    pD = p1 - p2
+    return Point(round(pD[0]/pScreenX, 4), round(pD[1]/pScreenY, 4), round(pD[2]/pScreenX, 4), round(pD[3]/pScreenY, 4))
 
 def shakiness(pD):
     #정도 조정. 임의적으로 10%으로 정함.
-    limit = 10
+    limit = 0.01 * 10
+    #print("[pDiff] " + str(pD))
     if (abs(pD.x) > limit or abs(pD.y) > limit or abs(pD.w) > limit or abs(pD.h) > limit):
-        print("[Shakiness] 흔들림!")
+        return "over"
+        #print("[Shakiness] 흔들림!")
     else:
-        print("[Shakiness] 가만함...")
+        return "under"
+        #print("[Shakiness] OK...")
+
