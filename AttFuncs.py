0ca7adc2bd55d4375c28d320663e4c08e314464f
kpureisen <96420192+kpureisen@users.noreply.github.com>
2022-04-23 20:42:16 +0900

    Add files via upload

---
+++ AttFuncs.py
@@ -0,0 +1,24 @@
+from Point import Point
+
+#화면크기: 720p
+pScreenX = 1280
+pScreenY = 720
+
+#차이% 계산
+def pDiff(p1, p2):
+    pD = p1 - p2
+    return Point(round(pD[0]/pScreenX, 4), round(pD[1]/pScreenY, 4), round(pD[2]/pScreenX, 4), round(pD[3]/pScreenY, 4))
+
+def shakiness(pD):
+	#정도 조정. 임의적으로 10%으로 정함.
+	limit = 0.01 * 10
+	#print("[pDiff] " + str(pD))
+	if (pD !=None):
+		if (abs(pD.x) > limit or abs(pD.y) > limit or abs(pD.w) > limit or abs(pD.h) > limit):
+        			return "over"
+        			#print("[Shakiness] 흔들림!")
+		else:
+        			return "under"
+        			#print("[Shakiness] OK...")
+	else:
+		return "None"
---
+++ Point.py
@@ -0,0 +1,36 @@
+#좌표
+class Point:
+    def __init__(self, x=0, y=0, w=0, h=0):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+         
+    #str(p), print(p) -> '(x=1, y=2, w=3, h=4)'
+    def __str__(self):
+        return '(x={0}, y={1}, w={2}, h={3})'.format(self.x, self.y, self.w, self.h)
+    
+    #pA+pB
+    def __add__(self, other):
+        return self.x + other.x, self.y + other.y, self.w + other.w, self.h + other.h
+    
+    #pA-pB
+    def __sub__(self, other):
+        return self.x - other.x, self.y - other.y, self.w - other.w, self.h - other.h
+    
+    def __truediv__(self, other):
+        #pA > pB
+        pA = Point(self.x if self.x > other.x else other.x,
+                   self.y if self.y > other.y else other.y,
+                   self.w if self.w > other.w else other.w,
+                   self.h if self.h > other.h else other.h)
+        pB = Point(self.x if pA.x == other.x else other.x,
+                   self.y if pA.y == other.y else other.y,
+                   self.w if pA.w == other.w else other.w,
+                   self.h if pA.h == other.h else other.h)
+       
+        #div/0 에러 방지. 곧 다시 불릴 것을 대비하여 True면 전 수치를 이용.
+        if pB.x == 0 or pB.y == 0 or pB.w == 0 or pB.h == 0:
+            return self
+        else:
+            return pA.x / pB.x, pA.y / pB.y, pA.w / pB.w, pA.h / pB.h
